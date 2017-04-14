5d995e74c279af4c87ceb0ebcb4dca6691722396
Derrick Schneider <derrickschneider@Derricks-MacBook-Pro.local>
2017-04-13 22:42:14 -0700

    Render master template in the extension popup

--- loadChecklist.js
+++ loadChecklist.js
@@ -2,6 +2,9 @@
  *  below
  */
 
+ // TODOS:
+ //   check object data in all places
+
 const loader = {
 
   /** Loads the checklist for the URL.
@@ -9,13 +12,12 @@ const loader = {
    *  @param {Element} the div that will contain the checklist items
    */
    loadChecklistForURL: function(url, checklistElem) {
-
      // determine if the URL is a key in the storage
 
      // determine if the URL is a prefix of a key in storage
 
      // configure a copy of the master template for this URL
-     console.log('loadChecklistForURL');
+
      fetch(chrome.extension.getURL('master_template.json')).then( response =>
        response.json().then(
          json => loader.loadChecklistFromJSON(json, checklistElem)));
@@ -30,9 +32,62 @@ const loader = {
         return;
       }
 
-      json.checklist.forEach( item => console.log(item));
-    }
+      json.checklist.forEach( item => checklistElem.append(loader.renderChecklistItem(item)));
+    },
+
+    /** For a given checklist item, generate the appropriate view in the Element.
+     *  @param {Object} json representing the checklist item to render
+     *  @return {Element} DOM element that can be appended
+     */
+     renderChecklistItem: function(checklistItem, containerElem) {
+       const itemElem = document.createElement('div');
+       const tableElem = document.createElement('table');
+       const tableRowElem = document.createElement('tr');
+
+       const checkboxElem = loader.getTdWrappedElement(loader.createCheckboxForChecklistItem(checklistItem));
+       const textElem = loader.getTdWrappedElement(loader.createTextForChecklistItem(checklistItem));
+       
+       tableRowElem.append(checkboxElem);
+       tableRowElem.append(textElem);
+       tableElem.append(tableRowElem);
+       itemElem.append(tableElem);
+
+       return itemElem;
+     },
+
+     /** Create the checkbox Element for the given checklistItem.
+      *  @ param {Object} json representing the checklistItem
+      *  @ return Element representing the checkbox
+      */
+      createCheckboxForChecklistItem: function(checklistItem) {
+        const checkboxElem = document.createElement('input');
+        checkboxElem.id = checklistItem.name;
+        checkboxElem.setAttribute("type", "checkbox")
+        return checkboxElem;
+      },
+
+      /** Create the text for the given checklist item.
+       *  @param {Object} the json representing the checklist item
+       *  @return {Element} a DOM element containing the text for the checklist item
+       */
+       createTextForChecklistItem: function(checklistItem) {
+         const textDivElem = document.createElement('div');
+         const itemText = document.createTextNode(checklistItem.name);
+         textDivElem.append(itemText);
+         return textDivElem;
+       },
+
+       /** Wrap the given element in a td element.
+        * @param {Element} the element to wrap in a td
+        * @return {Element} a td element containing the passed element
+        */
+        getTdWrappedElement: function(elementToWrap) {
+          td = document.createElement('td');
+          td.append(elementToWrap);
+          return td;
+        }
 }
 
 //loader.clearChecklistDiv();
-loader.loadChecklistForURL() // todo: add URL
+const checklistElem = document.getElementById('checklist');
+loader.loadChecklistForURL('nourl', checklistElem); // todo: add URL
